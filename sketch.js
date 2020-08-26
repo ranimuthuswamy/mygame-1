6ae2f4bb9ae2c60923c937d0ecfc6beb98c51c22
ranimuthuswamy <67144212+ranimuthuswamy@users.noreply.github.com>
2020-08-26 21:53:15 +0530

    Fixed Issue of input field

--- sketch.js
+++ sketch.js
@@ -1,36 +1,30 @@
 const START=0;
 const PLAY=1;
 const END=2;
+
 var bg,formBg
 var form,game;
 var gameState=START;
-
 
 function preload(){
 bg=loadImage("../download.jpg");
 formBg=loadImage("../download-_1_.png");
 }
 
-
 function setup() {
   createCanvas(displayWidth-10,displayHeight-200);
   //createSprite(400, 200, 50, 50);
-  
-  
+  game = new Game();
+  game.start();
 }
 
 function draw() {
+ // background(bg);
 // if(gameState===START){
 //   game.start();
 // }
 //  if(gameState===PLAY){
-// // game.play();
-
+//   game.play();
 // }
-form=new Form();
-form.display();
-  
- 
-
   drawSprites();
 }
